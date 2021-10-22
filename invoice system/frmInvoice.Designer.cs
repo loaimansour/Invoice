2171ca1c4ea726118b98b28b4655dc20d9958ddc
loai saad <loai.saad84@outlook.com>
2021-10-22 23:21:15 +0300

    first commit

---
+++ invoice system/frmInvoice.Designer.cs
@@ -0,0 +1,385 @@
+﻿
+namespace invoice_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInvNum = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cpxItem = new System.Windows.Forms.ComboBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dgvInvoice = new System.Windows.Forms.DataGridView();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.colItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(576, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "billing screen";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::invoice_system.Properties.Resources.Invoice_BILLING;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label2.Location = new System.Drawing.Point(42, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "www.Invoice.com";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Invoice number :\r\n";
+            // 
+            // txtInvNum
+            // 
+            this.txtInvNum.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtInvNum.Location = new System.Drawing.Point(439, 125);
+            this.txtInvNum.Name = "txtInvNum";
+            this.txtInvNum.Size = new System.Drawing.Size(444, 29);
+            this.txtInvNum.TabIndex = 4;
+            this.txtInvNum.Text = "000000";
+            this.txtInvNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDate
+            // 
+            this.txtDate.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtDate.Location = new System.Drawing.Point(439, 171);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(444, 29);
+            this.txtDate.TabIndex = 6;
+            this.txtDate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtDate.TextChanged += new System.EventHandler(this.txtDate_TextChanged);
+            this.txtDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDate_KeyPress);
+            this.txtDate.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtDate_MouseDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(369, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Date :";
+            // 
+            // txtName
+            // 
+            this.txtName.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtName.Location = new System.Drawing.Point(439, 212);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(444, 29);
+            this.txtName.TabIndex = 8;
+            this.txtName.Text = "Name";
+            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            this.txtName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtName_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(297, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Client Name :";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(46, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(850, 41);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "__________________________________________________________________________";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 24);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Item:";
+            // 
+            // cpxItem
+            // 
+            this.cpxItem.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cpxItem.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cpxItem.FormattingEnabled = true;
+            this.cpxItem.Location = new System.Drawing.Point(89, 293);
+            this.cpxItem.Name = "cpxItem";
+            this.cpxItem.Size = new System.Drawing.Size(277, 32);
+            this.cpxItem.TabIndex = 11;
+            this.cpxItem.SelectedIndexChanged += new System.EventHandler(this.cpxItem_SelectedIndexChanged);
+            this.cpxItem.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cpxItem_KeyDown);
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.BackColor = System.Drawing.Color.White;
+            this.txtPrice.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtPrice.Location = new System.Drawing.Point(89, 340);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.ReadOnly = true;
+            this.txtPrice.Size = new System.Drawing.Size(163, 29);
+            this.txtPrice.TabIndex = 13;
+            this.txtPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 343);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 24);
+            this.label8.TabIndex = 12;
+            this.label8.Text = " Price:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtQuantity.Location = new System.Drawing.Point(345, 340);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(163, 29);
+            this.txtQuantity.TabIndex = 15;
+            this.txtQuantity.Text = "1";
+            this.txtQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtQuantity.TextChanged += new System.EventHandler(this.txtQuantity_TextChanged);
+            this.txtQuantity.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtQuantity_KeyDown);
+            this.txtQuantity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtQuantity_KeyPress);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(261, 343);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 24);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Quantity:";
+            // 
+            // dgvInvoice
+            // 
+            this.dgvInvoice.AllowUserToAddRows = false;
+            this.dgvInvoice.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvInvoice.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvInvoice.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInvoice.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colItem,
+            this.colQty,
+            this.colPrice,
+            this.Total});
+            this.dgvInvoice.Location = new System.Drawing.Point(46, 390);
+            this.dgvInvoice.Name = "dgvInvoice";
+            this.dgvInvoice.Size = new System.Drawing.Size(837, 144);
+            this.dgvInvoice.TabIndex = 16;
+            this.dgvInvoice.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dgvInvoice_CellBeginEdit);
+            this.dgvInvoice.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInvoice_CellEndEdit);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BackColor = System.Drawing.Color.RoyalBlue;
+            this.txtTotal.ForeColor = System.Drawing.Color.White;
+            this.txtTotal.Location = new System.Drawing.Point(607, 338);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(163, 29);
+            this.txtTotal.TabIndex = 18;
+            this.txtTotal.Text = "0";
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTotal.TextChanged += new System.EventHandler(this.txtTotal_TextChanged);
+            this.txtTotal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTotal_KeyPress);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(545, 341);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 24);
+            this.label10.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(517, 341);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(89, 24);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Total--->";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(780, 321);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 45);
+            this.btnAdd.TabIndex = 20;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // colItem
+            // 
+            this.colItem.HeaderText = "Item";
+            this.colItem.Name = "colItem";
+            this.colItem.ReadOnly = true;
+            // 
+            // colQty
+            // 
+            this.colQty.HeaderText = "Quantity";
+            this.colQty.Name = "colQty";
+            // 
+            // colPrice
+            // 
+            this.colPrice.HeaderText = "unit price";
+            this.colPrice.Name = "colPrice";
+            this.colPrice.ReadOnly = true;
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "SubTotal";
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(984, 546);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dgvInvoice);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cpxItem);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtInvNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Invoice Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInvNum;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cpxItem;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dgvInvoice;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colQty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+    }
+}
+
---
+++ invoice system/frmInvoice.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace invoice_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.Invoice.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (DataGridViewColumn d in dgvInvoice.Columns)
+            {
+
+                d.DefaultCellStyle.ForeColor = Color.Red;
+            }
+            dgvInvoice.Columns[3].DefaultCellStyle.BackColor = Color.Blue;
+
+
+            txtDate.Text = DateTime.Now.ToString("dd/mm/yyyy");
+            Dictionary<int, string> itemData = new Dictionary<int, string>();
+            itemData.Add(350, "Laptop Dell");
+            itemData.Add(400, "Laptop Samsung");
+            itemData.Add(750, "Laptop Toshipa");
+            itemData.Add(300, "Laptop Acer");
+            itemData.Add(1050, "Laptop Mac");
+            itemData.Add(409, "Laptop Lenovo");
+            itemData.Add(55, "Printer HP");
+            itemData.Add(23, "mouse Dell");
+
+
+
+
+            cpxItem.DataSource = new BindingSource(itemData, null);
+            cpxItem.DisplayMember = "value";
+            cpxItem.ValueMember = "key";
+            txtPrice.Text = cpxItem.SelectedValue.ToString();
+
+
+
+
+            txtName.Focus();
+            txtName.Select();
+            txtName.SelectAll();
+        }
+
+        private void txtDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void txtDate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDate_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                txtDate.ContextMenu = new ContextMenu();
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTotal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void txtName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                cpxItem.Focus();
+
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cpxItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtPrice.Text = cpxItem.SelectedValue.ToString();
+        }
+
+        private void cpxItem_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                txtQuantity.Focus();
+                txtQuantity.SelectAll();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cpxItem.SelectedIndex <= -1) return;
+
+            string c = cpxItem.Text;
+            string pr = txtPrice.Text;
+            string qu = txtQuantity.Text;
+            int reslt = int.Parse(txtPrice.Text) * int.Parse(txtQuantity.Text);
+            txtTotal.Text = reslt.ToString();
+            string t = txtTotal.Text;
+            object[] arr = { c, qu, pr, t };
+
+            dgvInvoice.Rows.Add(arr);
+
+            int T = 0;
+            foreach (DataGridViewRow r in dgvInvoice.Rows)
+            {
+                T += Convert.ToInt32(r.Cells["Total"].Value);
+
+            }
+            txtTotal.Text = T.ToString();
+
+
+
+
+
+        }
+
+        private void txtQuantity_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+
+
+                int result = int.Parse(txtPrice.Text) * int.Parse(txtQuantity.Text);
+                txtTotal.Text = result.ToString();
+
+
+
+                cpxItem.Focus();
+            }
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtQuantity_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+
+            }
+
+        }
+
+        string oldqty = "1";
+        private void dgvInvoice_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            if (dgvInvoice.CurrentRow != null)
+            {
+                oldqty = dgvInvoice.CurrentRow.Cells["colQty"].Value.ToString();
+            }
+        }
+
+        private void dgvInvoice_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvInvoice.CurrentRow != null)
+            {
+                string newQty = dgvInvoice.CurrentRow.Cells["colQty"].Value+"";
+
+                if (oldqty == newQty) return;
+                if (!Regex.IsMatch(newQty, "^\\d+$"))
+                {
+                    dgvInvoice.CurrentRow.Cells["colQty"].Value = oldqty;
+
+                }
+                else
+                {
+                   
+                    int p = Convert.ToInt32(dgvInvoice.CurrentRow.Cells["colPrice"].Value);
+                    int r = Convert.ToInt32(newQty);
+                    dgvInvoice.CurrentRow.Cells["Total"].Value = (p*r);
+
+                    int T = 0;
+                    foreach (DataGridViewRow d in dgvInvoice.Rows)
+                    {
+                        T += Convert.ToInt32(d.Cells["Total"].Value);
+
+                    }
+                    txtTotal.Text = T.ToString();
+
+                }
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+        }
+    }
+}
